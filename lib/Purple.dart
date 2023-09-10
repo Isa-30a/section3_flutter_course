4cda7781ea853c77d7ebadb59c4c5c2d29beb7ad
Isa-30a <activator1274@gmail.com>
2023-09-10 10:16:44 -0500

    center Purple class

--- lib/Purple.dart
+++ lib/Purple.dart
@@ -7,44 +7,46 @@ class Purple extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 300,
-          height: 300,
-        ),
-        const SizedBox(height: 100),
-        Text(
-          message,
-          style: const TextStyle(
-            fontSize: 30,
-            color: Colors.white,
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            height: 300,
           ),
-        ),
-        const SizedBox(height: 20),
-        OutlinedButton(
-          onPressed: () {
-            print('yes');
-          },
-          style: OutlinedButton.styleFrom(
-            foregroundColor: Colors.white,
-            backgroundColor: const Color.fromARGB(255, 131, 19, 151),
-            padding: const EdgeInsets.symmetric(
-              horizontal: 40,
-              vertical: 20,
-            ),
-          ),
-          child: const Text(
-            'Start Quiz',
-            style: TextStyle(
-              fontSize: 20,
+          const SizedBox(height: 100),
+          Text(
+            message,
+            style: const TextStyle(
+              fontSize: 30,
               color: Colors.white,
             ),
           ),
-        ),
-      ],
+          const SizedBox(height: 20),
+          OutlinedButton(
+            onPressed: () {
+              print('yes');
+            },
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(255, 131, 19, 151),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40,
+                vertical: 20,
+              ),
+            ),
+            child: const Text(
+              'Start Quiz',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,16 +11,15 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body: Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(colors: [
-              Color.fromRGBO(57, 13, 255, 0.412),
-              Color.fromRGBO(166, 13, 255, 0.412)
-            ])),
-            child: Purple('Learn Flutter the fun way!'),
-          ),
-          backgroundColor: const Color.fromARGB(255, 131, 19, 151)),
-    );
+        home: Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+          Color.fromRGBO(57, 13, 255, 0.412),
+          Color.fromRGBO(166, 13, 255, 0.412)
+        ])),
+        child: Purple('Learn Flutter the fun way!'),
+      ),
+    ));
   }
 }
